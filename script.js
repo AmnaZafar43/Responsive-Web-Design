1fb1007a8c3aab78a92afeceed8db82f68e4af2b
Amna Zafar <Amnazafar052@gmail.com>
2024-02-26 11:43:54 +0500

    Make Design Responsive
    Make my design responsive now it completely become eye-catching on any screen type.

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 let search = document.querySelector(".searchBar");
+let navbar = document.querySelector(".navbar");
+let menuIcon = document.querySelector("#menu-icon");
 
-document.querySelector('#search-icon').onclick = () => {
-    search.classList.toggle('active');
+document.querySelector("#search-icon").onclick = () => {
+  navbar.classList.remove("active");
+};
+
+menuIcon.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+function checkDocumentFocus(e) {
+  if (document.hasFocus()) {
+    document.title = "Coffee Shop";
+    // body.style.background = "white";
+  } else {
+    document.title = "Come Back";
+    // body.style.background = "gray";
+  }
 }
+
+window.addEventListener("focus", checkDocumentFocus);
+window.addEventListener("blur", checkDocumentFocus);
